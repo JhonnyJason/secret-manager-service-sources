57cdd18390e8149ba8d09696eb3a7018fcf33cbd
Lenard Frommelt <lenny09918050@googlemail.com>
2021-08-11 09:31:39 +0200

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,11 +1,9 @@
 module.exports = {
     getDependencies: () => {
         return {
-            "body-parser": "^1.19.0",
-            "express": "^4.17.1",
             "noble-ed25519": "^1.2.5",
             "secret-manager-crypto-utils": "0.0.7",
-            "systemd": "^0.4.0"
+            "thingy-sci-base": "^0.0.3"
         }
     }
 }
